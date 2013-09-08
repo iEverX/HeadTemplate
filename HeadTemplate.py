8ce74b3945afa1d2968b8f9e9dfaaa2a309af7c9
Ever <g.everax@gmail.com>
2013-09-08 19:21:00 +0800

    modify template; other small change

--- HeadTemplate.py
+++ HeadTemplate.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import re, os
@@ -7,11 +6,13 @@ import sublime, sublime_plugin
 PACKAGE_NAME = 'HeadTemplate'
 PACKAGES_PATH = sublime.packages_path()
 
+
 def get_syntax_name(s):
     r = r'Packages/.*/(.*).tmLanguage'
     match = re.match(r, s)
     if match:
         return match.group(1)
+
 
 def get_syntax_vars(view):
     s = sublime.load_settings('HeadTemplate.sublime-settings')
@@ -21,28 +22,34 @@ def get_syntax_vars(view):
 
 def get_tpl_file(settings, filename):
     template_root = os.path.join(
-                    PACKAGES_PATH, 
-                    PACKAGE_NAME, 
+                    PACKAGES_PATH,
+                    PACKAGE_NAME,
                     settings.get('template_root'))
     return os.path.join(template_root, filename)
+
+
+def check_start(v, flag):
+    return v.substr(0) == flag[0]
+
 
 class WriteHeader(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         s, syntax_name, syntax_settings = get_syntax_vars(view)
         if syntax_settings and \
             syntax_settings.get('on_save') and \
-            view.substr(0) != syntax_settings.get('first_character')[0]:
+            not check_start(view, syntax_settings.get('first_character')):
                 with open(get_tpl_file(s, '%s.tpl' % syntax_name)) as f:
                     template = f.read()
                 edit = view.begin_edit()
                 view.insert(edit, 0, template.strip() + '\n\n')
                 view.end_edit(edit)
 
+
 class WriteHeaderCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         s, syntax_name, syntax_settings = get_syntax_vars(self.view)
         if syntax_settings and \
-            self.view.substr(0) != syntax_settings.get('first_character')[0]:
+            not check_start(view, syntax_settings.get('first_character')):
             with open(get_tpl_file(s, '%s.tpl' % syntax_name)) as f:
                     template = f.read()
             self.view.insert(edit, 0, template.strip() + '\n\n')
